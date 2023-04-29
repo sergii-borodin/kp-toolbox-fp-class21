46cfef50f7b8965e39a49d8dea2bae49a1b5011b
BoroDinS <sergey.borodin.lg@gmail.com>
2023-04-29 15:07:25 +0200

    change knexfile.js production

--- packages/server/knexfile.js
+++ packages/server/knexfile.js
@@ -23,7 +23,7 @@ module.exports = {
       port: process.env.DB_PORT,
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
-      database: process.env.DB_DATABASE,
+      database: process.env.DB_NAME,
     },
     pool: { min: 0, max: 7 },
     seeds: {
