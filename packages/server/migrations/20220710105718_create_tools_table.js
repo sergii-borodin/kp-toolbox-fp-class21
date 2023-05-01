5ff6af57a797a8be124b96e9ae495c57d8ef2be3
BoroDinS <sergey.borodin.lg@gmail.com>
2023-05-01 16:29:38 +0200

    minimum in table

--- packages/server/migrations/20220710105718_create_tools_table.js
+++ packages/server/migrations/20220710105718_create_tools_table.js
@@ -4,23 +4,7 @@
  */
 exports.up = function (knex) {
   return knex.schema.createTable('tools', (table) => {
-    table.increments();
     table.string('name').notNullable();
-    table.integer('time_frame_min').notNullable(5);
-    table.integer('time_frame_max');
-    table.integer('group_size_min').notNullable(2);
-    table.integer('group_size_max');
-    table.string('facilitation_level').notNullable();
-    table.string('materials');
-    table.text('pitch');
-    table.text('description').notNullable();
-    table.json('instructions');
-    table.string('source');
-    table.string('picture');
-    table
-      .dateTime('created_at')
-      .notNullable()
-      .defaultTo(knex.raw('CURRENT_TIMESTAMP'));
   });
 };
 
