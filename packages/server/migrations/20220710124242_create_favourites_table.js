e6db88370ffa34775e3c5699fc5e3a2cd1c7e07b
BoroDinS <sergey.borodin.lg@gmail.com>
2023-05-01 18:25:50 +0200

    update

--- packages/server/migrations/20220710124242_create_favourites_table.js
+++ packages/server/migrations/20220710124242_create_favourites_table.js
@@ -6,9 +6,9 @@ exports.up = function (knex) {
   return knex.schema.createTable('favourites', (table) => {
     table.increments();
     table.integer('user_id').unsigned();
-    table.foreign('user_id').references('id').inTable('users');
+    table.foreign('user_id').references('users.id');
     table.integer('tool_id').unsigned();
-    table.foreign('tool_id').references('id').inTable('tools');
+    table.foreign('tool_id').references('tools.id');
   });
 };
 
