97330cd9a55901d1187acb074c13697516ef80f5
BoroDinS <sergey.borodin.lg@gmail.com>
2023-05-01 16:40:55 +0200

    change from timestamp to datetime

--- packages/server/migrations/20220710105718_create_tools_table.js
+++ packages/server/migrations/20220710105718_create_tools_table.js
@@ -4,7 +4,20 @@
  */
 exports.up = function (knex) {
   return knex.schema.createTable('tools', (table) => {
+    table.increments();
     table.string('name').notNullable();
+    table.integer('time_frame_min').notNullable(5);
+    table.integer('time_frame_max');
+    table.integer('group_size_min').notNullable(2);
+    table.integer('group_size_max');
+    table.string('facilitation_level').notNullable();
+    table.string('materials');
+    table.text('pitch');
+    table.text('description').notNullable();
+    table.json('instructions');
+    table.string('source');
+    table.string('picture');
+    table.datetime('created_At').defaultTo(knex.fn.now()).notNullable();
   });
 };
 
