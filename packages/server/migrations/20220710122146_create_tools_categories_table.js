22345f23ab4d872536c4cb6c6e699ba9741ccb55
BoroDinS <sergey.borodin.lg@gmail.com>
2023-05-01 17:02:44 +0200

    remove id

--- packages/server/migrations/20220710122146_create_tools_categories_table.js
+++ packages/server/migrations/20220710122146_create_tools_categories_table.js
@@ -4,7 +4,6 @@
  */
 exports.up = function (knex) {
   return knex.schema.createTable('tools_categories', (table) => {
-    table.increments();
     table.integer('tool_id').unsigned();
     table.foreign('tool_id').references('id').inTable('tools');
     table.integer('category_id').unsigned();
